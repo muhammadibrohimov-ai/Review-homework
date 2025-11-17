808859ba179b298281111bf9387095a47c481460
Muhammadjon Ibrohimov <miuceo@Muhammadjons-MacBook-Air.local>
2025-11-17 13:42:15 +0500

    added custom user

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'main',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 STATIC_URL = 'static/'
 
